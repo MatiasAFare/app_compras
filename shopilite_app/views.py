16258603c4a50821234cd1de28f5b62cbb75605a
MatiasAFare <matiasfare@gmail.com>
2024-11-26 23:36:03 -0300

    Postman testing

--- shopilite_app/views.py
+++ shopilite_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+import json
 # Create your views here.
 
 def index(request):
     return HttpResponse("Welcome to Shopilite App.")
 
+
+def about(request):
+    return JsonResponse({
+        "lucas"
+        : "cometravas",
+        "goldental"
+        : "comegatos"
+
+    })
