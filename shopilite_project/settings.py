8136645804633c83aef6beb1076662e967df540c
MatiasAFare <matiasfare@gmail.com>
2024-11-30 10:47:16 -0300

    removed djongo, added a dummy db for testing

--- shopilite_project/settings.py
+++ shopilite_project/settings.py
@@ -77,7 +77,14 @@ WSGI_APPLICATION = 'shopilite_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-
+    'default': {
+        'ENGINE': 'PyMongo',
+        'NAME': 'shopilite',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': DATABASE_URL,
+        }
+    }
 }
 
 # Password validation
