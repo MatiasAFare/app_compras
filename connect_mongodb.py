259c371e3f1daa3ffb27580a90c6c0f2ab1b0a86
MatiasAFare <matiasfare@gmail.com>
2024-12-01 20:05:53 -0300

    Created CRUD for Lucas G. spot

--- connect_mongodb.py
+++ connect_mongodb.py
@@ -1,12 +1,18 @@
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+from shopilite_app.schemas import producto
+
 
 load_dotenv()
 
 DB_URL = os.getenv('MONGODB_URL')
 client = MongoClient(os.getenv("DATABASE_URL"))
 db = client[os.getenv("DATABASE_NAME")]
+
+if 'productos' in db.list_collection_names():
+    print("Collection already exists")
+else:
+    productos_collection = db.create_collection("productos", validator = producto)
+
 productos_collection = db["productos"]
-
-
--- shopilite_app/views.py
+++ shopilite_app/views.py
@@ -1,45 +1,77 @@
+from datetime import datetime
 import json
+from logging import exception
+
+from bson import ObjectId
 from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
 from django.views.decorators.http import require_http_methods
+from bson.json_util import dumps
 from connect_mongodb import *
-
+from pymongo.errors import BulkWriteError
 
 
 def index(request):
     return HttpResponse("Welcome to Shopilite App.")
 
 
-
 @require_http_methods(["POST"])
 def add_product(request):
-        # Procesar el cuerpo del POST
-        body_unicode = request.body.decode('utf-8')
-        print("Contenido del POST:", body_unicode)
-        return JsonResponse({"message": "Método POST recibido correctamente"})
+    try:
+        product_data = json.loads(request.body)
+        if 'datetime' in product_data:
+            product_data['datetime'] = datetime.fromisoformat(product_data['datetime'].replace("Z", "+00:00"))
+        product_id = productos_collection.insert_one(product_data).inserted_id
+
+    except Exception as error:
+        # Capturar cualquier otro tipo de error
+        return JsonResponse({'error': 'Unknown error', 'message': str(error)}, status=500)
+
+    product_data['_id'] = str(product_id)
+    return JsonResponse(product_data)
 
 
+@require_http_methods(["GET"])
 def count_products(request):
     count = productos_collection.count_documents({})
     response_data = dict(Cantidad=count)
     return JsonResponse(response_data)
 
 
+@require_http_methods(["GET"])
 def get_products(request):
-     JsonResponse(productos_collection.find({}))
+    def serialize_document(document):
+        document['_id'] = str(document['_id'])
+        return document
 
+    try:
+        documents = list(productos_collection.find({}))
+        serialized_documents = [serialize_document(doc) for doc in documents]
+        return JsonResponse(serialized_documents, safe=False)
+
+    except Exception as error:
+        return JsonResponse({'error': 'Failed to retrieve products', 'message': str(error)}, status=500)
+
+
+@require_http_methods(["PUT"])
 def modify_product(request, product_id):
-    return JsonResponse(productos_collection.update_one({"id": product_id}))
+    product_data = json.loads(request.body)
+    product = productos_collection.find_one_and_update({"_id": ObjectId(product_id)},
+                                                       {"$set": {product_data}})
+    print(product)
+    product['_id'] = str(product['_id'])
+    return JsonResponse(product)
 
+
+@require_http_methods(["DELETE"])
 def delete_product(request, product_id):
-    return JsonResponse(productos_collection.delete_one({"id": product_id}))
+    return HttpResponse(productos_collection.delete_one({"_id": ObjectId(product_id)}))
+
 
 def get_product_by_id(request, product_id):
-    product = productos_collection.find_one({"id": product_id})
+    product = productos_collection.find_one({"_id": ObjectId(product_id)})
+    print(product)
+    product['_id'] = str(product['_id'])
     if product:
         return JsonResponse(product)
     else:
         return JsonResponse({"error": "Product not found."}, status=404)
-
-
-
-
