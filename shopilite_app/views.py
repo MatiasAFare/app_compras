dcf0858347ed77c3b04a0aca7930f041845b7530
MatiasAFare <matiasfare@gmail.com>
2024-12-01 14:20:19 -0300

    Third World Country problems

--- shopilite_app/views.py
+++ shopilite_app/views.py
@@ -1,37 +1,31 @@
-from django.http import HttpResponse, JsonResponse
+import json
+from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
+from django.views.decorators.http import require_http_methods
 from connect_mongodb import *
 
-
-# Create your views here.
 
 
 def index(request):
     return HttpResponse("Welcome to Shopilite App.")
 
 
-def add_product(product):
-    required_fields = ["product_name", "price", "establishment_name", "marca", "datetime"]
-    for field in required_fields:
-        if field not in product:
-            print(f"Error: The {field} field is mandatory and it's missing.")
-            return
 
-    if productos_collection.find_one({"productoId": product.get("productoId")}):
-        print(f"Error: The ID product {product['productoId']} already exist.")
-        return
-
-    try:
-        result = productos_collection.insert_one(product)
-        return JsonResponse(result)
-    except Exception as e:
-        return JsonResponse({"error": {e}}, status=500)
+@require_http_methods(["POST"])
+def add_product(request):
+        # Procesar el cuerpo del POST
+        body_unicode = request.body.decode('utf-8')
+        print("Contenido del POST:", body_unicode)
+        return JsonResponse({"message": "Método POST recibido correctamente"})
 
 
 def count_products(request):
-    return JsonResponse(productos_collection.count_documents({}))
+    count = productos_collection.count_documents({})
+    response_data = dict(Cantidad=count)
+    return JsonResponse(response_data)
+
 
 def get_products(request):
-    return JsonResponse(productos_collection.find({}))
+     JsonResponse(productos_collection.find({}))
 
 def modify_product(request, product_id):
     return JsonResponse(productos_collection.update_one({"id": product_id}))
